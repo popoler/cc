ddf3fbebb90b7828aeea6bfae967a85feabbece3
popoler <97371937+popoler@users.noreply.github.com>
2022-01-09 19:03:29 +0700

    Update cpu_hh.sh

--- cpu_hh.sh
+++ cpu_hh.sh
@@ -3,4 +3,5 @@ sudo apt install proxychains -y
 sed -i 's/socks4/socks5/' /etc/proxychains.conf
 sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
 sed -i 's/9050/10663/' /etc/proxychains.conf
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && proxychains ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RC3G7FfFYLxfnP5DYMKp5zkDWuJtofUXWq.wangsit -p x --cpu 8
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz
+proxychains ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RC3G7FfFYLxfnP5DYMKp5zkDWuJtofUXWq.wangsit -p x --cpu 8
