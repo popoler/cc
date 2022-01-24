4968eef386f9c4310378dda21abb1662d73e1409
popoler <97371937+popoler@users.noreply.github.com>
2022-01-24 22:07:29 +0700

    Update cpu_hh.sh

--- cpu_hh.sh
+++ cpu_hh.sh
@@ -5,4 +5,4 @@ sudo sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
 sudo sed -i 's/9050/10663/' /etc/proxychains.conf
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
-proxychains ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RC3G7FfFYLxfnP5DYMKp5zkDWuJtofUXWq.wangsit -p x --cpu 8
+proxychains ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RC3G7FfFYLxfnP5DYMKp5zkDWuJtofUXWq.wangsit -p x --cpu 2
